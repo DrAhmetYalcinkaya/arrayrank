14430b695852a03dd2f9f25323b3212e75581d9e
DrAhmetYalcinkaya <ahyalcinkaya@gmail.com>
2025-08-22 13:37:33 +0200

    Simplifying the simple to be simpler
    simple_process.R is now simpler and uses the assemble.output function. The older methods still work.

--- simple_process.R
+++ simple_process.R
@@ -2,10 +2,6 @@ if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install("limma")
 
-library(devtools)
-remove.packages("arrayrank")
-detach("package:arrayrank", unload = TRUE)
-
 devtools::install_github("DrAhmetYalcinkaya/arrayrank")
 library(arrayrank)
 
@@ -22,22 +18,12 @@ discordants <- discordant(raw_data, array_type = "huprot", fold = 4, abs = 2000)
 corr_df <- replace.low(wide_df, offset = 10)
 qnorm_df <- multinorm(corr_df, method= "ig")
 
-# 3. storage and backup
-qstore_df <- store.df(qnorm_df)
-writexl::write_xlsx(qstore_df, "your_storage_file.xlsx")
+# 3. data assembly and storage
+keyfile <- readxl::read_xlsx(choose.files())
+stored_data <- assemble.output(keyfile, wide_df,corr_df,qnorm_df)
 
-# 4. read data from backup file
-q_data <- readxl::read_xlsx(choose.files())
+# 4. selection of dataset to be analyzed and removal of sample ID row
+q_data <- stored_data[["Norm"]][-2,]
 
-# 5. read group info from excel (if you want to provide a vector defining the sample groups)
-groups <- readxl::read_xlsx(choose.files())
-groupsv <- t(groups[,2]) ### note: read/create/edit the character vector based on your own requirements!!!
-
-#IMPORTANT: Either add group info into the first row of the backup data (your_file_name.xlsx for this example) manually,
-#or create a separate vector/excel file with the group info for each sample. Code accounts for both methods.
-
-# 6. detect proteins with hits and rank them (in this example: the groups have been provided as a vector)
-q_result <- detect.hits(q_data, controls = "your_control_group", group_vector = groupsv,
-                        examine = "your_study_group", sdmean = 0.5, fold_threshold = 5, absolute_threshold = 5000)
-
-# If group data were added into the backup data file (so there is no 'groups' vector), remove "group_vector = groups" before running.
+# 5. detect proteins with hits and rank them
+q_result <- detect.hits(q_data, controls = "BD", examine = "APS1", sdmean = 0.5, fold_threshold = 5, absolute_threshold = 5000)
